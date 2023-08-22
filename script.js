4dd00efb6303fdbc7a70b018a66921ddb60652de
KeineAngabe32 <142903753+KeineAngabe32@users.noreply.github.com>
2023-08-22 20:18:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 function updateResults() {
     const parties = [
         { name: 'cdu', likelihood: 0.35 },
-        { name: 'spd', likelihood: 0.15 },
         { name: 'greens', likelihood: 0.20 },
-        { name: 'fdp', likelihood: 0.10 },
-        { name: 'afd', likelihood: 0.20 }
+        { name: 'spd', likelihood: 0.15 },
+        { name: 'afd', likelihood: 0.10 },
+        { name: 'fdp', likelihood: 0.20 }
     ];
+
+    let totalPercentage = 0;
 
     parties.forEach(partyObj => {
         const element = document.querySelector(`.${partyObj.name}`);
-        const randomPercentage = generateRandomPercentage(partyObj.likelihood);
+        const randomPercentage = generateRandomPercentage(partyObj.likelihood, totalPercentage);
+        totalPercentage += randomPercentage;
         element.textContent = `${partyObj.name.toUpperCase()} - ${randomPercentage}%`;
     });
 }
 
-function generateRandomPercentage(likelihood) {
-    const maxPercentage = 50; // Maximum percentage to generate
+function generateRandomPercentage(likelihood, remainingPercentage) {
+    const maxPercentage = 100 - remainingPercentage;
     const threshold = Math.random() * maxPercentage * likelihood;
     return Math.floor(Math.random() * threshold + (1 - likelihood) * maxPercentage);
 }
